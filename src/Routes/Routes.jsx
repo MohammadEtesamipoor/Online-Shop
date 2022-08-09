ba0a0ef385a3adac258f80d1482965d20fc7ff71
Mohammad Etesamipoor <Mohammad.etesamipoor78@gmail.com>
2022-08-10 01:51:55 +0430

    Complete project routing (add public and private route)

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { PAGE } from "Configs/route";
 
 import {
@@ -13,29 +18,57 @@ import {
   AdminProductPage,
 } from "Pages";
 
-import { OutletRouter } from "./OutletRouter";
+import { PublicRoute } from "./PublicRoute";
 import Layout from "Layouts/Layout";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const Routing = () => {
   return (
     <Router>
       <Routes>
         
-        <Route element={<OutletRouter />}  >
+        <Route element={<PublicRoute />}>
           <Route path={PAGE.Main} element={<Layout main={<HomePage />} />} />
           <Route element={<Layout main={<HomePage />} />} />
-          <Route path={PAGE.Home} element={<Navigate replace to='/' />} />
-          <Route path={PAGE.Products} element={<Layout main={<ProductsPage />} />} />
-          <Route path={PAGE.Product}element={<Layout main={<ProductPage />} />} />
-          <Route path={PAGE.Basket}element={<Layout main={<BasketPage />} />}  />
-          <Route path={PAGE.Checkouts}element={<Layout main={<CheckoutsPage />} />} />
-          <Route path={PAGE.Login}element={<Layout main={<LoginPage />} />} />
+          <Route path={PAGE.Home} element={<Navigate replace to="/" />} />
+          <Route
+            path={PAGE.Products}
+            element={<Layout main={<ProductsPage />} />}
+          />
+          <Route
+            path={PAGE.Product}
+            element={<Layout main={<ProductPage />} />}
+          />
+          <Route
+            path={PAGE.Basket}
+            element={<Layout main={<BasketPage />} />}
+          />
+          <Route
+            path={PAGE.Checkouts}
+            element={<Layout main={<CheckoutsPage />} />}
+          />
         </Route>
-        <Route>
-          <Route path={PAGE.Admin_Product} element={<AdminProductPage />} />
-          <Route path={PAGE.Admin_Order} element={<AdminOrdersPage />} />
-          <Route path={PAGE.Admin_Quantity} element={<AdminQuantityPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path={PAGE.Admin_Product}
+            element={<Layout main={<AdminProductPage />} />}
+          />
+          <Route
+            path={PAGE.Admin_Order}
+            element={<Layout main={<AdminOrdersPage />} />}
+          />
+          <Route
+            path={PAGE.Admin_Quantity}
+            element={<Layout main={<AdminQuantityPage />} />}
+          />
         </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path={PAGE.Login} element={<Layout main={<LoginPage />} />} />
+        </Route>
+
       </Routes>
     </Router>
   );
