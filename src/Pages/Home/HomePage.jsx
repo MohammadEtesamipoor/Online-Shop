49e70f35d9b7068ce814dff1c523dbb9e1e840a7
Mohammad Etesamipoor <Mohammad.etesamipoor78@gmail.com>
2022-08-17 12:02:22 +0430

    Create product card component

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,9 +1,7 @@
 import { GetProductsCategory } from "apis/ApiCategory";
 import { GetProducts } from "apis/ApiProduct";
 import { useEffect, useState } from "react";
-import { FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
+import {ProductCard} from 'Components'
 import {
   Box,
   Skeleton,
@@ -42,101 +40,7 @@ export function HomePage() {
                 {productData.map(
                   (itemProduct) =>
                     itemProduct["category-id"] == itemCategory["id"] && (
-                      <Box
-                        _hover={{
-                          transform: "translateY(-10px)",
-                          boxShadow: "3xl",
-                         
-                        }}
-                        role={"group"}
-                        p={6}
-                        maxW={"330px"}
-                        w={"full"}
-                        boxShadow={"2xl"}
-                        rounded={"lg"}
-                        pos={"relative"}
-                        zIndex={1}
-                      >
-                        <Box
-                          rounded={"lg"}
-                          mt={-12}
-                          pos={"relative"}
-                          height={"230px"}
-                          _after={{
-                            transition: "all .3s ease",
-                            content: '""',
-                            w: "full",
-                            h: "full",
-                            pos: "absolute",
-                            top: 5,
-                            left: 0,
-                            backgroundImage: `url(${require(`upload/${itemProduct.images[0]}`)})`,
-                            filter: "blur(15px)",
-                            zIndex: -1,
-                          }}
-                          _groupHover={{
-                            _after: {
-                              filter: "blur(20px)",
-                            },
-                          }}
-                        >
-                          <Box
-                  
-                          >
-                            <div
-                              style={{
-                                backgroundImage: `url(${require(`upload/${itemProduct.images[0]}`)})`,
-                                backgroundRepeat: "no-repeat",
-                                backgroundSize: " contain, cover",
-                                backgroundPosition: "bottom",
-                                width: "282px",
-                                height: "230px",
-                              }}
-                            ></div>
-                          </Box>
-                        </Box>
-                        <Stack pt={10} align={"center"}>
-                          <Text
-                            color={"gray.500"}
-                            fontSize={"sm"}
-                            textTransform={"uppercase"}
-                          >
-                            {itemCategory["name-en"]}
-                          </Text>
-                          <Heading
-                            fontSize={"2xl"}
-                            fontFamily={"body"}
-                            fontWeight={500}
-                          >
-                            {itemProduct["product-name-fa"]}
-                          </Heading>
-                          <Stack direction={"row"} align={"center"}>
-                            <Link
-                            to="/product"
-                            >
-                            
-                            <Box
-                              _hover={{
-                                transform: "translateY(10px)",
-                                color: "#6a64c4",
-                              }}
-                            >
-                              <FaShoppingCart
-                              fontSize="50px" />
-                            </Box>
-                            </Link>
-                            <Text fontWeight={800} fontSize={"xl"}>
-                              {itemProduct["price"]}
-                            </Text>
-                            <Text
-                              textDecoration={"line-through"}
-                              color={"gray.600"}
-                            >
-                              {itemProduct["price"] * 1.2}
-                            </Text>
-                          </Stack>
-                        </Stack>
-                      </Box>
+                      <ProductCard itemProduct={itemProduct} itemCategory={itemCategory} />
                     )
                 )}
               </Box>
